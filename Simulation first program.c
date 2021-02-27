72cffdde2391cdb8a33542996c6b178ab370b885
shfx0096 <68342255+shfx0096@users.noreply.github.com>
2021-02-27 13:47:48 +0600

    Add files via upload

---
+++ Simulation first program.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main()
+{
+    int n,i,sum,ia[500],a[500];
+    a[1]=0;
+    printf("Enter the number of customer: ");
+    scanf("%d",&n);
+    printf("Enter the Inter arrival time: ");
+    for(i=1;i<=n;i++){
+        scanf("%d",&ia[i]);
+    }
+    for(i=2;i<=n;i++){
+        a[i]=ia[i]+a[i-1];
+    }
+    printf("Arrival time: ");
+    for(i=1;i<=n;i++){
+        printf("%d ",a[i]);
+    }
+
+}
